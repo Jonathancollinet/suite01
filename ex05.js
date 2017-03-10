9d3a7772fa219da5c1f56f5c7a40e3a748213870
Amaurea <aurore.chatagnier@gmail.com>
2017-03-10 18:49:06 +0100

    fonction qui nous dit si le chiffre est pair ou impair

--- ex05.js
+++ ex05.js
@@ -24,3 +24,10 @@ isEvenConcat(6);
 isEvenConcat(5);
 
 //  écrire votre code sous ce commentaire
+
+function isEvenConcat(nbr){
+    if(nbr%2 == 0){
+        return console.log(nbr + ' est pair');
+    }
+    else return console.log(nbr + ' est impair');
+}
