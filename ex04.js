3a83d95474b7829b673315f86ee5e6a4bdcc7e5c
Piotr-V <pierrevicq@hotmail.fr>
2017-03-14 14:52:52 +0100

    ex04 done

--- ex04.js
+++ ex04.js
@@ -24,3 +24,14 @@ isEven(6);
 isEven(5);
 
 //  écrire votre code sous ce commentaire
+
+function isEven(n) {
+    if (n % 2 == 0) {
+        return 1;
+        }
+        if (n % 2 != 0) {
+            return 0;
+        }
+}
+console.log(isEven(6));
+console.log(isEven(5));
