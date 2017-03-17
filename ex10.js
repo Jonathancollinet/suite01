52978fe73718498641739eab4cb836f7a351d806
Jean-Paul REMAN <jpolair@gmail.com>
2017-03-17 16:45:51 +0100

    avec une partie de l'historique...

--- ex10.js
+++ ex10.js
@@ -45,3 +45,76 @@ shoppingList([
 ]);
 
 //  écrire votre code sous ce commentaire
+/*
+function shoppingList(arrayOfFruits) {
+    var num = 0;
+    for (groupeFruit of arrayOfFruits) {
+        console.log(groupeFruit);
+        for (fruit of groupeFruit) {
+            //console.log(fruit);
+        }
+    }
+}
+*/
+
+/*function shoppingList(arrayOfFruits) {
+    var table = [];
+    var objFruit = new Object;
+    var num = 0;
+    for (let i = 0; i < arrayOfFruits.length; i++) {
+        for (let j = 0; j < arrayOfFruits.length-1; j++) {
+           // var property = arrayOfFruits[i][j];
+            //objFruit[property] =0;
+            objFruit[arrayOfFruits[i][j]] = 0;
+            //table.push(property);
+            //table.sort();    
+            for (var prop in objFruit) {
+    
+                if (objFruit.hasOwnProperty(prop)) {
+                    objFruit[arrayOfFruits[i][j]] +=1;
+                } else {
+                    objFruit[arrayOfFruits[i][j]] = 1;
+                }
+            }
+
+        }   
+    }
+    console.log(table);
+    console.log(objFruit);
+    console.log(table.length);
+    return table;
+    
+}*/
+/*
+function shoppingList(arrayOfFruits) {
+    var table = [];
+    var table2 = [];
+    var num = 0;
+    var obj = new Object();
+    var obj2 = new Object();
+    obj2.property = 0;
+
+    for (var i = 0; i < arrayOfFruits.length; i++) {
+        for (var j = 0; j < arrayOfFruits.length - 1; j++) {
+            table.push(arrayOfFruits[i][j]);
+           
+           
+        }
+    }
+    console.log(table);
+    console.log(obj);
+}
+*/
+function shoppingList(arrayOfFruits) {
+    var obj = new Object();
+    for (var i = 0; i < arrayOfFruits.length; i++) {
+        for (var j = 0; j < arrayOfFruits.length - 1; j++) {
+            if (obj[arrayOfFruits[i][j]] != undefined ) {
+                obj[arrayOfFruits[i][j]]++;
+            } else {
+                obj[arrayOfFruits[i][j]] = 1;
+            } 
+        }
+    }
+    console.log(obj);
+}
